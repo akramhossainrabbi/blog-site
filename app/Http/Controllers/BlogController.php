413f7a49d7be10eba8dc498e152c4cd7350e63f7
Coder-fox-bd <ahrabbi121@gmail.com>
2022-10-18 00:54:36 +0600

    first commit

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Blog::all();
+    
+        return view('posts.index', compact('posts'));
+    }
+   
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title'=>'required',
+            'body'=>'required',
+        ]);
+    
+        Blog::create($request->all());
+    
+        return redirect()->route('posts.index');
+    }
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Blog::find($id);
+        return view('posts.show', compact('post'));
+    }
+}
---
+++ app/Http/Livewire/CommentsDisplay.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CommentsDisplay extends Component
+{
+    public function render()
+    {
+        return view('livewire.comments-display');
+    }
+}
